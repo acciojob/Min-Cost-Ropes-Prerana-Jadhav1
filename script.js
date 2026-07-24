bf9ea04e26ff5d99534eabe982815c13534548ec
Prerana-Jadhav1 <preranajb5@gmail.com>
2026-07-24 09:49:51 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,18 @@
+function mincost(arr) { 
+	let cost=0;
+
+	while (arr.length>1) {
+		arr.sort((a,b)=>a-b);
+
+		let first=arr.shift();
+		let second=arr.shift();
+
+		let sum=first+second;
+
+		const+=sum;
+	}
+
+	return cost;
+}
+
+module.exports=mincost;
